75df128fc624b462364188b2f2edcce3d7408506
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-04-16 05:19:54 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -46,9 +46,72 @@ async def callback(bot, msg):
         )
 
     elif msg.data == "abou":
+        reply1 = await query.message.reply_text(
+            text="⭗ ⭗ ⭗ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply2 = await reply1.edit_text(
+            text="⦿ ⭗ ⭗ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply3 = await reply2.edit_text(
+            text="⦿ ⦿ ⭗ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply4 = await reply3.edit_text(
+            text="⦿ ⦿ ⦿ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply5 = await reply4.edit_text(
+            text="⦿ ⦿ ⦿ ⦿ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply6 = await reply5.edit_text(
+            text="⦿ ⦿ ⦿ ⦿ ⦿ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply7 = await reply6.edit_text(
+            text="⦿ ⦿ ⦿ ⦿ ⦿ ⦿"
+        )
+        await asyncio.sleep(0.5)
+        await reply7.delete()
         await msg.message.edit(
             text="""<b>✮ 𝙼𝚈 𝙽𝙰𝙼𝙴: 𝚁𝙴𝚇
 ✮ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: 𝚉𝙸𝙽𝙰𝙽
 ✮ 𝚂𝙿𝙴𝙲𝙸𝙰𝙻 𝚃𝙷𝙰𝙽𝙺𝚂 𝚃𝙾: <a href=https://t.me/Mo_Tech_YT>𝙼𝙾 𝚃𝙴𝙲𝙷</a>
 ✮ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✮ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹</b>"""
+
+    elif msg.data == "help":
+        reply1 = await query.message.reply_text(
+            text="⭗ ⭗ ⭗ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply2 = await reply1.edit_text(
+            text="⦿ ⭗ ⭗ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply3 = await reply2.edit_text(
+            text="⦿ ⦿ ⭗ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply4 = await reply3.edit_text(
+            text="⦿ ⦿ ⦿ ⭗ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply5 = await reply4.edit_text(
+            text="⦿ ⦿ ⦿ ⦿ ⭗ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply6 = await reply5.edit_text(
+            text="⦿ ⦿ ⦿ ⦿ ⦿ ⭗"
+        )
+        await asyncio.sleep(0.5)
+        reply7 = await reply6.edit_text(
+            text="⦿ ⦿ ⦿ ⦿ ⦿ ⦿"
+        )
+        await reply7.delete()
+        await msg.message.edit(
+            text="𝙷𝙴𝙻𝙿 ഒന്നും ഇല്ല ഓടിക്കൊ"
+            reply_markup=InlineKeyboardMarkup( [[
+                Inl
