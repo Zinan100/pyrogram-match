7501144bc0ce6110e5e6d11b70c747f2f752ab52
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-04-16 10:38:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
-pyrogram = Client
+pyrogram = Client(
     "PYROGRAM MATCH"
     api_id=API_ID, 
     api_hash=API_HASH,
